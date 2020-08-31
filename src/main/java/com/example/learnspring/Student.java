454d6f58e9328ffc2f9ca46823bda4a468fb0c55
23520055542@qq.com <2352005542@qq.com>
2020-08-31 11:29:52 +0800

    spring jdbc框架

--- src/main/java/com/example/learnspring/Student.java
+++ src/main/java/com/example/learnspring/Student.java
@@ -3,6 +3,14 @@ package com.example.learnspring;
 public class Student {
     private Integer age;
     private String name;
+    private Integer id;
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+    public Integer getId() {
+        return id;
+    }
     public void setAge(Integer age) {
         this.age = age;
     }
