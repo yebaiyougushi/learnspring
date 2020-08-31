384a1ee033b3f641a609bdaf807229e7b2fabf4e
23520055542@qq.com <2352005542@qq.com>
2020-08-31 10:36:11 +0800

    spring框架的AOP“

--- src/main/java/com/example/learnspring/LearnspringApplication.java
+++ src/main/java/com/example/learnspring/LearnspringApplication.java
@@ -1,19 +1,17 @@
 package com.example.learnspring;
 
-import com.example.learnspring.collection.JavaCollection;
-import com.example.learnspring.main.HelloWorld;
-import com.example.learnspring.main.TextEditor;
-import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.ApplicationContext;
 import org.springframework.context.support.AbstractApplicationContext;
 import org.springframework.context.support.ClassPathXmlApplicationContext;
 
+
+
 @SpringBootApplication
 public class LearnspringApplication {
 
     public static void main(String[] args) {
-        AbstractApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
+         ApplicationContext context = new ClassPathXmlApplicationContext("application.xml");
 //        HelloWorld obj = (HelloWorld) context.getBean("helloWorld");
 //
 //        obj.getMessage();
@@ -22,12 +20,20 @@ public class LearnspringApplication {
 //        TextEditor te = (TextEditor) context.getBean("textEditor");
 //        te.spellCheck();
 //        注入集合
-
-        JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
-        jc.getAddressList();
-        jc.getAddressSet();
-        jc.getAddressMap();
-        jc.getAddressProp();
+//
+//        JavaCollection jc = (JavaCollection) context.getBean("javaCollection");
+//        jc.getAddressList();
+//        jc.getAddressSet();
+//        jc.getAddressMap();
+//        jc.getAddressProp();
+//        Profile profile = (Profile) context.getBean("profile");
+//        profile.printAge();
+//        profile.printName();
+//        AOP
+        Student student = (Student) context.getBean("student");
+        student.getName();
+        student.getAge();
+        student.printThrowException();
     }
     //test
 }
--- src/main/java/com/example/learnspring/main/TextEditor.java
+++ src/main/java/com/example/learnspring/main/TextEditor.java
@@ -1,16 +1,22 @@
 package com.example.learnspring.main;
 public class TextEditor {
     private SpellChecker spellChecker;
+    private String name;
     public TextEditor(){
 
     }
-    public TextEditor(SpellChecker spellChecker) {
-//        System.out.println("Inside TextEditor constructor." );
-//        this.spellChecker = spellChecker;
+
+    public void setSpellChecker( SpellChecker spellChecker ){
+       // this.spellChecker = spellChecker;
     }
-    public void setSpellChecker(SpellChecker spellChecker) {
-        System.out.println("Inside setSpellChecker." );
-        this.spellChecker = spellChecker;
+    public SpellChecker getSpellChecker() {
+        return spellChecker;
+    }
+    public void setName(String name) {
+        this.name = name;
+    }
+    public String getName() {
+        return name;
     }
     public void spellCheck() {
         spellChecker.checkSpelling();
